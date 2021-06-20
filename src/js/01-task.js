845d7cecda97b32d8a1874a408ea4eb5cc6e81a5
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-06-20 08:38:50 +0300

    hw11-03

--- src/js/01-task.js
+++ src/js/01-task.js
@@ -1,4 +1,10 @@
-const delay = ms => Promise.resolve(ms);
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
 
 const logger = time => console.log(`Resolved after ${time}ms`);
 
